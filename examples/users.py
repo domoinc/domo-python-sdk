17b8693489c6d33f92d1b1e9d8824d1b9c21bb73
Robert Watts <robert.watts@domo.com>
2017-08-15 11:24:24 -0600

    Fixed indent

--- examples/users.py
+++ examples/users.py
@@ -4,40 +4,39 @@ from pydomo.users import CreateUserRequest
 
 
 def users_examples(domo):
-        '''User Docs: https://developer.domo.com/docs/domo-apis/users
-        '''
-        domo.logger.info("\n**** Domo API - User Examples ****\n")
+    '''User Docs: https://developer.domo.com/docs/domo-apis/users
+    '''
+    domo.logger.info("\n**** Domo API - User Examples ****\n")
 
-        # Build a User
-        user_request = CreateUserRequest()
-        user_request.name = 'Leonhard Euler'
-        user_request.email = 'leonhard.euler{}@domo.com'.format(
-                                                             randint(0, 10000))
-        user_request.role = 'Privileged'
-        send_invite = False
+    # Build a User
+    user_request = CreateUserRequest()
+    user_request.name = 'Leonhard Euler'
+    user_request.email = 'leonhard.euler{}@domo.com'.format(randint(0, 10000))
+    user_request.role = 'Privileged'
+    send_invite = False
 
-        # Create a User
-        user = domo.users.create(user_request, send_invite)
-        domo.logger.info("Created User '{}'".format(user['name']))
+    # Create a User
+    user = domo.users.create(user_request, send_invite)
+    domo.logger.info("Created User '{}'".format(user['name']))
 
-        # Get a User
-        user = domo.users.get(user['id'])
-        domo.logger.info("Retrieved User '" + user['name'] + "'")
+    # Get a User
+    user = domo.users.get(user['id'])
+    domo.logger.info("Retrieved User '" + user['name'] + "'")
 
-        # List Users
-        user_list = domo.users.list(10, 0)
-        domo.logger.info("Retrieved a list containing {} User(s)".format(
+    # List Users
+    user_list = domo.users.list(10, 0)
+    domo.logger.info("Retrieved a list containing {} User(s)".format(
                                                                len(user_list)))
 
-        # Update a User
-        user_update = CreateUserRequest()
-        user_update.name = 'Leo Euler'
-        user_update.email = 'leo.euler{}@domo.com'.format(randint(0, 10000))
-        user_update.role = 'Privileged'
-        user = domo.users.update(user['id'], user_update)
-        domo.logger.info("Updated User '{}': {}".format(user['name'],
-                                                        user['email']))
+    # Update a User
+    user_update = CreateUserRequest()
+    user_update.name = 'Leo Euler'
+    user_update.email = 'leo.euler{}@domo.com'.format(randint(0, 10000))
+    user_update.role = 'Privileged'
+    user = domo.users.update(user['id'], user_update)
+    domo.logger.info("Updated User '{}': {}".format(user['name'],
+                                                    user['email']))
 
-        # Delete a User
-        domo.users.delete(user['id'])
-        domo.logger.info("Deleted User '{}'".format(user['name']))
+    # Delete a User
+    domo.users.delete(user['id'])
+    domo.logger.info("Deleted User '{}'".format(user['name']))
