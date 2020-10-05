22df9e1c36b807b8c8a4061766582dc5f658ef1b
Roy Keyes <rkeyes@meteoreducation.com>
2020-10-05 15:43:20 -0400

    Add query a dataset to client

--- pydomo/DomoAPIClient.py
+++ pydomo/DomoAPIClient.py
@@ -60,6 +60,15 @@ class DomoAPIClient(object):
                               + self.transport.dump_response(response))
             raise Exception(obj_desc + " Error: " + response.text)
 
+    def _query(self, url, request, params, obj_desc):
+        response = self.transport.post(url=url, params=params, body=request)
+        if response.status_code == requests.codes.OK:
+            return response.json()
+        else:
+            self.logger.debug("Error querying " + obj_desc + ": "
+                              + self.transport.dump_response(response))
+            raise Exception("Error querying " + obj_desc + ": " + response.text)
+
     def _delete(self, url, obj_desc):
         response = self.transport.delete(url=url)
         if response.status_code == requests.codes.NO_CONTENT:
